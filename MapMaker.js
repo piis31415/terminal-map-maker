83c446f72acdbdcec9bc656cebd55f723492577b
kevinbai0 <kevin.c.bai0@gmail.com>
2018-12-10 15:46:17 -0500

    Added comments

--- MapMaker.js
+++ MapMaker.js
@@ -11,6 +11,7 @@ import {groupColors, colorNames} from "./helpers/groupColors";
 import CustomHead from "./components/CustomHead";
 
 class MapMaker extends React.Component {
+    // initializes an empty representation of the map
     getInitGameMap() {
         let gameMap = [];
         for (let i = 0; i < 28; i++) {
@@ -26,6 +27,12 @@ class MapMaker extends React.Component {
                 else gameMap[i].push({ type: ItemType.ME, colorIndex: 0});
             }
         }
+        /* 
+        gameMap is a 2-d array with each item containing an object: {
+            type: ItemType // defines if a destructor, encryptor etc. is placed down.
+            colorIndex: Number (integer) // it's index defines the group that it is in.
+        }
+        */
         return gameMap;
     }
     constructor(props) {
@@ -43,18 +50,18 @@ class MapMaker extends React.Component {
     render() {
         let firewallItemClass = "firewall-item";
         let firewallItemSelected = "firewall-item firewall-item-selected";
-        let groupColor = groupColors[this.state.selectedGroupIndex];
+        let groupColor = groupColors[this.state.selectedGroupIndex]; // defines what colour to render each new object
         return (
             <div className="map-maker-container">
                 <CustomHead />
                 {
+                    // modal for showing the user's points
                     this.state.showModal && <div className="modal">
                         <section className="modal-content">
                             <div className="title">My points</div>
                             <div className="results-field">{
                                 this.state.coordinateStrings
                                     .map((string, i) => {
-
                                         return <div key={i} className="points-group">{string.split("\n").map(str => <div>{str}</div>)}</div>
                                     })   
                             }</div>
@@ -62,9 +69,13 @@ class MapMaker extends React.Component {
                         </section>
                     </div>
                 }
+                { /* Game Map */ }
                 <GameMap gameMap={this.state.gameMap} mapItemClick={this.mapItemClick} mapItemHover={this.mapItemHover}/>
+                
+                { /* Options panel */}
                 <div className="map-maker-options-container">
                     <div className="container-title">Options</div>
+                    { /* Firewalls container to choose which type of firewall to place on map */ }
                     <div className="firewall-title">Choose Placement Marker</div>
                     <div className="firewalls-container options-container">
                         <div 
@@ -92,6 +103,7 @@ class MapMaker extends React.Component {
                             <div className="x" style={{color: groupColor}}>X</div>
                         </div>
                     </div>
+                    { /* Each of the colors group - lets user pick which color to use */ }
                     <div className="groups-container options-container">
                         {
                             this.state.groups.map((group, i) => <GroupItem 
@@ -103,15 +115,19 @@ class MapMaker extends React.Component {
                         }
                         <div className="group-item add-group-item" onClick={() => this.addGroup()}><div>+</div></div>
                     </div>
+                    { /* Utility buttons (get all points and remove all points) */ }
                     <div className="export-points-container options-container">
                         <div className="utility-button" onClick={() => this.getPointsAsCode()}>Get Points</div>
                         <div className="utility-button" onClick={() => this.resetMap()}>Remove All Points</div>
                     </div>
+                    { /* Displays the current coordinate the user is hovering on */ }
                     <div className="current-coordinate-container options-container">
                         <div className="current-coordinate-title">Current Coordinate</div>
                         <div className="current-coordinate-value">{this.state.currentHoveredCoordinate && this.state.currentHoveredCoordinate.x + ", " + this.state.currentHoveredCoordinate.y}</div>
                     </div>
                 </div>
+
+                {/* Styles imported from stylesheets */}
                 {mapMakerStyle}
                 {mapItemStyle}
             </div>
@@ -119,24 +135,29 @@ class MapMaker extends React.Component {
     }
     
     mapItemClick = (coordinate) => {
+        // convert the x,y coordinates to i,j for indexing the array
         let i = 27 - coordinate.y;
         let j = coordinate.x;
+        
+        // only execute if the selected coordinate is a valid coordinate or represents a change
         if (this.state.firewallType === undefined) return;
         if (this.state.gameMap[i][j].type === ItemType.NON_EXISTENT || this.state.gameMap[i][j].type === ItemType.ENEMY) return;
-
-        if (this.state.gameMap[i][j].type === this.state.firewallType && this.state.gameMap[i][j].colorIndex === this.state.selectedGroupIndex) {
-            return;
-        }
+        if (this.state.gameMap[i][j].type === this.state.firewallType && this.state.gameMap[i][j].colorIndex === this.state.selectedGroupIndex) return;
+        
+        // create copy of the map
         let mapCopy = this.state.gameMap;
+        // change the coordinate selected to current selected options
         mapCopy[i][j] = {
             type: this.state.firewallType,
             colorIndex: this.state.selectedGroupIndex
         }
+        // update state (which updates the map)
         this.setState({
             gameMap: mapCopy
         })
     }
     mapItemHover = (coordinate) => {
+        // updates the current hovered coordinate so the user knows which coordinate is hovered over
         if (this.state.currentHoveredCoordinate && coordinate.x === this.state.currentHoveredCoordinate.x && coordinate.y === this.state.currentHoveredCoordinate.y) return;
         this.setState({
             currentHoveredCoordinate: coordinate
@@ -144,6 +165,7 @@ class MapMaker extends React.Component {
     }
 
     setFirewallItem(itemType) {
+        // when the user selects a specific item in the firewall options panel, update the state
         if (this.state.firewallType === itemType) return;
         this.setState({
             firewallType: itemType
@@ -151,6 +173,7 @@ class MapMaker extends React.Component {
     }
 
     setSelectedGroup(index) {
+        // when the user selects a specific item in the groups options panel, update the state
         if (index === this.state.selectedGroupIndex) return;
         this.setState({
             selectedGroupIndex: index
@@ -158,6 +181,7 @@ class MapMaker extends React.Component {
     }
 
     addGroup() {
+        // add a new group if not all groups are already added
         if (this.state.groups.length < groupColors.length) {
             let groups = this.state.groups;
             groups.push(groupColors[this.state.groups.length]);
@@ -168,12 +192,14 @@ class MapMaker extends React.Component {
         }
     }
 
+    // reset the map to start fresh and new mapping
     resetMap() {
         this.setState({
             gameMap: this.getInitGameMap()
         })
     }
 
+    // get the currently selected points and format into string to be outputted for the user
     getPointsAsCode() {
         let {gameMap} = this.state;
 
@@ -196,7 +222,6 @@ class MapMaker extends React.Component {
 
         // converting coordinates to string
         coordinatesAsStrings = coordinatesAsStrings.map((obj, colorIndex) => {
-            let newObj = {}
             let masterString = "";
             for (let item in obj) {
                 let val = obj[item]
@@ -212,12 +237,9 @@ class MapMaker extends React.Component {
                     masterString += colorNames[colorIndex] + "_" + typeString + "_points = " + val;
                 }
             }
-            console.log(masterString);
             return masterString;
         });
 
-        // next, reduce coordinate strings
-        
         this.setState({
             showModal: true,
             coordinateStrings: coordinatesAsStrings
